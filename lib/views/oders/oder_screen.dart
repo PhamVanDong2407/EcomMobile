c05c1f9f12f35dde7cf1da93db0ef46ee392c2d9
DongKey03 <dongpham24703@gmail.com>
2025-03-08 09:11:02 +0700

    update UI oder_screen

--- lib/views/oders/oder_screen.dart
+++ lib/views/oders/oder_screen.dart
@@ -11,6 +11,7 @@ class _OderScreenState extends State<OderScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Color(0xff1D182A),
       body: Padding(
         padding: EdgeInsets.symmetric(
           horizontal: 25,
@@ -18,11 +19,70 @@ class _OderScreenState extends State<OderScreen> {
         child: SafeArea(
           child: Column(
             children: [
-
+              SizedBox(height: 30),
+              textTitle(),
+              Expanded(
+                child: Center(
+                  child: btnOder(),
+                ),
+              ),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class textTitle extends StatelessWidget {
+  const textTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Oders',
+      style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+    );
+  }
+}
+
+class btnOder extends StatefulWidget {
+  const btnOder({super.key});
+
+  @override
+  State<btnOder> createState() => _btnOderState();
+}
+
+class _btnOderState extends State<btnOder> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min, // Đảm bảo nội dung chỉ chiếm không gian cần thiết
+      children: [
+        Image.asset('assets/images/check-out 1.png'),
+        const SizedBox(height: 30),
+        const Text(
+          'No Orders yet',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+        ),
+        const SizedBox(height: 30),
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xff8E6CEF),
+            minimumSize: const Size(200, 50),
+          ),
+          child: const Text(
+            'Explore Categories',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
